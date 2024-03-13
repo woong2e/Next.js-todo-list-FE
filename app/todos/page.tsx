815652551e6e58b9366ffc15184f52d998111339
woong2e <tlswodnd12345@naver.com>
2024-03-13 14:40:29 +0900

    content-type 체크 코드 추가

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -5,16 +5,22 @@ async function fetchTodosApiCall() {
 	const response = await fetch(`${process.env.BASE_URL}/api/todos`,
 	 { cache: 'no-store' });
 
+	const contentTypeHeaderValue = response.headers.get('content-type');
+
+	if (contentTypeHeaderValue?.includes("text/html")) {
+		return null;
+    }
 	return response.json();
 }
 
 export default async function TodosPage() {
 	const response = await fetchTodosApiCall();
-	console.log(response.data);
+
+	const fetchedTodos = response.data ?? [];
 	return (	
 		<div className="flex flex-col space-y-8">
 			<h1 className={title()}>Todos</h1>
-			<TodosTable todos={response.data}/>
+			<TodosTable todos={fetchedTodos}/>
 		</div>
 	);
 }
