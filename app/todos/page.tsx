4c72142227029694ec9ad14bfb3130c748bff036
woong2e <tlswodnd12345@naver.com>
2024-03-13 17:17:48 +0900

    수정5

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -2,12 +2,10 @@ import { title } from "@/components/primitives";
 import { TodosTable } from "@/components/todos-table";
 
 async function fetchTodosApiCall() {
-	const response = await fetch(`${process.env.BASE_URL}/api/todos`,
+	const response = await fetch(`${process.env.BASE_URL}/api/todos/`,
 	 { cache: 'no-store' });
 
-	 console.log("response: ", response);
 	const contentTypeHeaderValue = response.headers.get('content-type');
-	console.log("contentTypeHeaderValue: ", contentTypeHeaderValue);
 
 	if (contentTypeHeaderValue?.includes("text/html")) {
 		console.log("contentTypeHeaderValue: ", contentTypeHeaderValue);
@@ -19,7 +17,7 @@ async function fetchTodosApiCall() {
 export default async function TodosPage() {
 	const response = await fetchTodosApiCall();
 
-	const fetchedTodos = response.data ?? [];
+	const fetchedTodos = response?.data ?? [];
 	return (	
 		<div className="flex flex-col space-y-8">
 			<h1 className={title()}>Todos</h1>
